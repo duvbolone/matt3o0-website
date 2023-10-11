7a708eafc8ff615680a6051fa15fa4195011c1f1
Matt3o0 <duvbolone@gmail.com>
2023-10-11 19:56:13 +0200

    feat: switch back to deleteMany

--- app/api/redirects/cleanup/route.tsx
+++ app/api/redirects/cleanup/route.tsx
@@ -23,28 +23,15 @@ export async function GET(request: NextRequest) {
         const db = client.db(dbName);
         const collection = db.collection("redirects");
         const epoch = Math.floor(Date.now() / 1000);
-        const result = await collection.find().toArray();
-        if (result[0]) {
-            for (let i = 0; i < result.length; i++) {
-                if (result[i].stops_on <= epoch || result[i].stops_on !== 1) {
-                    await collection.deleteOne({ redirect_id: result[i].redirect_id})
-                }
-            }
-        } else {
-            return new NextResponse(JSON.stringify({
-                status: 500,
-                message: 'MongoDB error.',
-                result: result
-            }), {
-                status: 500,
-                headers: { 'Content-Type': 'application/json' },
-            });
-        }
-
+        const deleteCriteria = {
+            stops_on: { $lte: epoch, $ne: 1 }
+          };
+          
+        const result = await collection.deleteMany(deleteCriteria);
         return new NextResponse(JSON.stringify({
             status: 200,
             message: 'Successfully removed all old redirects.',
-            result: result
+            time: epoch
         }), {
             status: 200,
             headers: { 'Content-Type': 'application/json' },
